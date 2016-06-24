995462a9d7e05f351ba0a7ddc4721fc05ed8f09f
Cesar <cesars90_@hotmail.com>
2016-06-23 23:39:20 -0500

    Implementada seccion Lideres sin denunciar

--- ReciclaMas/app/src/main/java/itesm/mx/reciclamas/Activities/LideresActivity.java
+++ ReciclaMas/app/src/main/java/itesm/mx/reciclamas/Activities/LideresActivity.java
@@ -1,20 +1,30 @@
 package itesm.mx.reciclamas.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import itesm.mx.reciclamas.Adapters.LideresListViewAdapter;
 import itesm.mx.reciclamas.R;
+import itesm.mx.reciclamas.Values.Database;
 
 public class LideresActivity extends AppCompatActivity {
     private TextView userTextView;
     private Button volverButton;
+    private ListView lideresListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class LideresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lideres);
         userTextView = (TextView) findViewById(R.id.bienvenidoTV);
         volverButton = (Button) findViewById(R.id.volverBttn);
+        lideresListView = (ListView) findViewById(R.id.lideresLV);
 
         // agregar el nombre del usuario al header
         userTextView.setText("Bienvenido, " + ParseUser.getCurrentUser().getUsername());
@@ -29,9 +40,14 @@ public class LideresActivity extends AppCompatActivity {
         volverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // terminar la actividad
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
+        final LideresListViewAdapter miAdaptador = new LideresListViewAdapter(getApplicationContext(),R.layout.row,getOrderedUserList());
+        lideresListView.setAdapter(miAdaptador);
     }
 
     @Override
@@ -54,5 +70,18 @@ public class LideresActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public List<ParseUser> getOrderedUserList(){
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.addDescendingOrder(Database.User.CANTIDAD);
+        List<ParseUser> users = new ArrayList<ParseUser>();
+        try {
+           users = query.find();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return users;
     }
 }
---
+++ ReciclaMas/app/src/main/java/itesm/mx/reciclamas/Adapters/LideresListViewAdapter.java
@@ -0,0 +1,50 @@
+package itesm.mx.reciclamas.Adapters;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import itesm.mx.reciclamas.R;
+import itesm.mx.reciclamas.Values.Database;
+
+/**
+ * Created by Cesar on 6/23/2016.
+ */
+public class LideresListViewAdapter extends ArrayAdapter<ParseUser> {
+    private Context context;
+    int layoutResourceId;
+    List<ParseUser> listaUsers;
+
+
+    public LideresListViewAdapter(Context context, int resource, List<ParseUser> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.layoutResourceId = resource;
+        this.listaUsers = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(layoutResourceId, parent, false);
+        }
+        TextView posUsuarioTextView = (TextView) row.findViewById(R.id.pos_usuarioTV);
+        Button denunciarButton = (Button) row.findViewById(R.id.denunciarBttn);
+
+        posUsuarioTextView.setText((position + 1) + ".- " + this.listaUsers.get(position).getUsername()
+                + "| kg: " + new DecimalFormat("#.##").format(this.listaUsers.get(position).get(Database.User.CANTIDAD)));
+        return row;
+    }
+}
